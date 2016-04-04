8efa3ab9e1236236534906a92164af4b1b9a6403
Conor Horan-Kates <conor.code@gmail.com>
2016-04-03 20:32:53 -0700

    adding php support via curl - which strangely fails, while curl itself does not

---
+++ test/php/http_get/test.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+
+<?php
+# h/t http://stackoverflow.com/a/17058314
+
+$meta_url = 'http://localhost:4567/meta';
+
+# TODO isn't this just shelling out to curl? god this language is absurd
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$meta_url);
+
+$meta_content = curl_exec($ch);
+$urls = explode(',', $meta_content);
+
+foreach ($urls as $url) {
+  $url = str_replace('[', '', $url);
+  $url = str_replace(']', '', $url);
+  $url = str_replace('"', '', $url);
+  echo "-> $url\n";
+  curl_setopt($ch, CURLOPT_URL, $url);
+  if (curl_exec($ch) == false) {
+    # TODO make this actually expose the error message
+    echo "ERROR: " . curl_error($ch);
+    exit(1);
+  }
+
+}
+
+exit(0);
+?>
---
+++ test/php/http_get/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# dependencies check
+if [ -z `which php` ]; then
+  echo "ERROR: unable to find 'curl' in \$PATH, skipping"
+  exit 0
+fi
+
+# test definition/execution
+TEST="`dirname $0`/test.php"
+echo "running [${TEST}]"
+php ${TEST}
