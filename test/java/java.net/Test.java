f8a57138a4206aa4bccd9f6f6a4640f4563cc725
Conor Horan-Kates <conor.code@gmail.com>
2016-04-10 16:47:15 -0700

    pattern in place, need to finish calling urls

---
+++ test/java/java.net/Test.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.*;
+
+public class Test {
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        String meta = getHTML("http://localhost:4567/meta");
+        System.out.println(meta);
+    }
+
+}
